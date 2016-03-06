c3de1a704cc09b41f1b5a3b08dbfb9943de5cf05
rlbayeta2589 <usefakename@gmail.com>
2016-03-06 16:44:36 +0800

    Added Documentation

--- bio_info.py
+++ bio_info.py
@@ -1,3 +1,13 @@
+################################################################################################/
+#
+#   bio_info.py
+#
+#   Author : Bayeta Reynaldo, III
+#            CMSC128 AB-1L
+#            https://github.com/rlbayeta2589
+#
+#################################################################################################/
+
 def getHammingDistance(str1, str2):
     len1 = len(str1)
     len2 = len(str2)
@@ -9,8 +19,8 @@ def getHammingDistance(str1, str2):
         return 'Error! Strings are not equal!'
 
     for i in range(len1):
-        if str1[i] != str2[i]:
-            dist += 1
+        if str1[i] != str2[i]:                  # increment distance value
+            dist += 1                           # if not chars is not equal
 
     return dist
 
@@ -24,26 +34,26 @@ def countSubstrPattern(original, pattern):
     
     for i in range(orig_len):
         index = i + ptrn_len
-        if orig_len < index:
-            return count
+        if orig_len < index:                    # if the length of the index
+            return count                        # exceeds the length of orig
         elif original[i:index]==pattern:
-            count += 1
-
+            count += 1                          # if the substring is equal to
+                                                # the pattern
 def isValidString(str, alphabet):
     strlen = len(str)
     if (strlen <= 0): 
         return 'Error! Length must never be 0 or negative!'
     
     for i in range(strlen):
-        if alphabet.find(str[i])==-1:
-            return False
+        if alphabet.find(str[i])==-1:           # .find function returns -1
+            return False                        #  if substring is not found
 
     return True
 
 def getSkew(str, n):
     if n <= 0 or n > len(str):
-        return 'Error! Invalid index!'
-
+        return 'Error! Invalid index!'          # returns the count of 'G' minus
+                                                #   the count of 'C'
     return (str.count('G',0,n) - str.count('C',0,n))
 
 def getMaxSkewN(str, n):
@@ -52,18 +62,20 @@ def getMaxSkewN(str, n):
     if n <= 0 or n > len(str):
         return 'Error! Invalid index!'
 
-    for i in range(n):
-        skews.append(getSkew(str, i+1))
+    for i in range(n):                          #  call getSkew function 'n' times
+        skews.append(getSkew(str, i+1))         #      and put the values in an array
 
-    return max(skews)
+    return max(skews)                           #  return the max in the array of skews 
 
 def getMinSkewN(str, n):
     skews = []
 
-    if n <= 0 or n > len(str):
-        return 'Error! Invalid index!'
+    if n <= 0 or n > len(str):                  #  call getSkew function 'n' times
+        return 'Error! Invalid index!'          #      and put the values in an array 
 
     for i in range(n):
         skews.append(getSkew(str, i+1))
 
-    return min(skews)
+    return min(skews)                           #  return the min in the array of skews 
+
+
